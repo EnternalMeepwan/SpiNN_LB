b977cc273bc4b5f506756668e88903e61987150f
Yuan Feng <33221812+YuaNFrank@users.noreply.github.com>
2020-07-26 18:35:21 +0100

    Update global.py

--- pythonCode/global.py
+++ pythonCode/global.py
@@ -1,11 +1,11 @@
+import math
+import random
+random.seed(100)
 runtime = 12000
 time_step = 1000
 time_scale_factor = 10
 cores_per_chip = 10
-_max_offset_factor = 0.2
-max_offset = int(time_step * time_scale_factor * _max_offset_factor)
 
-# generate the timer offset for a processor
+max_offset = 3000
 def generate_offset(processor):
-    return int(
-        math.ceil(max_offset / cores_per_chip) * processor)
+    return min(math.ceil((processor - random.random()) / (cores_per_chip+4) * max_offset), max_offset)
