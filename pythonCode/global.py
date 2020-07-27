cb46a95e0649542a6f9693c14970c54ba65828bb
Yuan Feng <33221812+YuaNFrank@users.noreply.github.com>
2020-07-27 01:26:11 +0100

    Update global.py

--- pythonCode/global.py
+++ pythonCode/global.py
@@ -3,9 +3,9 @@ import random
 random.seed(100)
 runtime = 12000
 time_step = 1000
-time_scale_factor = 10
+time_scale_factor = 5
 cores_per_chip = 10
 
-max_offset = 3000
+max_offset = 1500
 def generate_offset(processor):
     return min(math.ceil((processor - random.random()) / (cores_per_chip+4) * max_offset), max_offset)
