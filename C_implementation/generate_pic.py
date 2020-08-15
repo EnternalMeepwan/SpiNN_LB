28e7cc649fbe188e6b3da5a513d06584cd298041
Yuan_Feng <y.feng-28@sms.ed.ac.uk>
2020-08-15 20:01:33 +0100

    reform file struvture

---
+++ C_implementation/generate_pic.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(0, 127, 128)
+Y = np.linspace(0, 127, 128)
+data = pd.read_csv("Vorticity_spinn_128.csv")
+def f(X,Y):
+    return (data.iloc[X,Y])
+
+fig = plt.figure()
+plt.title('128 * 128 contour of Vorticity from SpiNNaker')
+ax = plt.subplot(1,1,1)
+plt.contour(X, Y, f(X,Y), 127, colors='k', linewidths=(0.7,))
+plt.contourf(X, Y, f(X,Y), 127, alpha=0.25, colors='black', linewidth=(0.7))
+ax.set_aspect('equal')
+plt.tight_layout()
+plt.show()
